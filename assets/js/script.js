8f357200c1f703141d37b68c9164dc2971d8aaf5
SanjaySingh <yogykasingh@gmail.com>
2022-02-06 18:36:27 +0530

    message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,61 @@
-var originalTitle = document.title;
-window.addEventListener('blur', function() {
-    document.title = '🌎 Travel With Pride';
+$(function() {
+    if ($(window).width() <= 1024) {
+        $('.hover-nav > a').click(function() {
+            $(this).toggleClass('expand');
+        });
+        $('.menu-opener').click(function() {
+            $(this).parents('#header').toggleClass('show');
+        });
+    }
 });
-window.addEventListener('focus', function() {
-    document.title = originalTitle;
-});
+let slideIndex = 0;
+showSlides();
+function nextSlide() {
+    slideIndex++;
+    showSlides();
+    timer = _timer;
+}
+function prevSlide() {
+    slideIndex--;
+    showSlides();
+    timer = _timer;
+}
+function currentSlide(n) {
+    slideIndex = n - 1;
+    showSlides();
+    timer = _timer;
+}
+function showSlides() {
+    let slides = document.querySelectorAll(".slide");
+    if (slideIndex > slides.length - 1) slideIndex = 0;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+        slide.classList.remove('active');
+    });
+    slides[slideIndex].style.display = "block";
+    slides[slideIndex].classList.add('active');
+}
+let timer = 7;
+const _timer = timer;
+setInterval(() => {
+    timer--;
+    if (timer < 1) {
+        nextSlide();
+        timer = _timer;
+    }
+}, 1000);
 
 const header = document.getElementById('header');
-const cookie = document.getElementById('cookie');
-
-const cookieDetailBtn = cookie.getElementsByClassName('detail')[0];
-cookieDetailBtn.addEventListener('click', cookieDetail);
-
-const cookieCloseBtn = cookie.getElementsByClassName('close')[0];
-cookieCloseBtn.addEventListener('click', cookieClose);
-
-const cookieAcceptBtn = cookie.getElementsByClassName('accept')[0];
-cookieAcceptBtn.addEventListener('click', cookieAccept);
-
 let lastScrollTop = 0;
 document.addEventListener('scroll', function(e) {
     var st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScrollTop) {
         console.log('scrolling-down');
-        if (st > header.offsetHeight) {
-            header.classList.remove('active');
-        }
+        header.classList.add('collapse');
     } else {
         console.log('scrolling-up');
-        header.classList.add('active');
+        header.classList.remove('collapse');
     }
     lastScrollTop = st <= 0 ? 0 : st;
 });
-
-function cookieDetail() {
-    cookie.classList.add('expand');
-}
-
-function cookieClose() {
-    cookie.classList.add('close');
-}
-
-function cookieAccept() {
-    cookie.classList.add('close').add('accept');
-}
